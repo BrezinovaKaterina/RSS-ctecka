0b1876a0c1e0079dfc1dc191ef7f2f0d8d7565f9
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-11-15 22:30:49 +0100

    Initial commit

---
+++ src/main/java/com/engeto/RSSctecka/RssCteckaApplication.java
@@ -0,0 +1,13 @@
+package com.engeto.RSSctecka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssCteckaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssCteckaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/engeto/RSSctecka/controller/ScifiController.java
@@ -0,0 +1,7 @@
+package com.engeto.RSSctecka.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScifiController {
+}
