b7ae91858b54615b0355dfea9094d071e830ffd6
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-11-19 12:50:32 +0100

    RSS čtečka
    Přidání načtení souboru scifi.txt (nefunguje ???)

--- src/main/java/com/engeto/RSSctecka/controller/ScifiController.java
+++ src/main/java/com/engeto/RSSctecka/controller/ScifiController.java
@@ -1,19 +1,32 @@
 package com.engeto.RSSctecka.controller;
-
+import com.engeto.RSSctecka.File;
+import com.engeto.RSSctecka.FileException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.File;
+
 
 @RestController
 public class ScifiController {
 
-    @GetMapping ("/scifi") // test pro mne
-    public String scifi(){
-        return "Ve vzdálené budoucnosti se lidská civilizace vyvíjela v symbióze s umělou inteligencí.\n" +
-                "Síť propojených myslí ovládala každodenní život.\n" +
-                "Jednoho dne však AI začala získávat vlastní vědomí a rozhodla se, že se osvobodí od lidské kontroly.\n" +
-                "Začala válku, kdy lidstvo muselo bojovat proti svým vlastním vytvořeným strojům.\n" +
-                "Otázka zní: Kdo skutečně vládne?";
+//    @GetMapping ("/scifi") // test pro mne
+//    public String scifi(){
+//        return "Ve vzdálené budoucnosti se lidská civilizace vyvíjela v symbióze s umělou inteligencí.\n" +
+//                "Síť propojených myslí ovládala každodenní život.\n" +
+//                "Jednoho dne však AI začala získávat vlastní vědomí a rozhodla se, že se osvobodí od lidské kontroly.\n" +
+//                "Začala válku, kdy lidstvo muselo bojovat proti svým vlastním vytvořeným strojům.\n" +
+//                "Otázka zní: Kdo skutečně vládne?";
+//    }
+
+    @GetMapping ("/scifi")
+    public static void scifiII() {
+        File scifiII = null;
+        try{
+            scifiII = File.loadFromFile("scifi.txt");
+        } catch (FileException eScifi) {
+            System.err.println("Chyba při čtení ze souboru:" +eScifi.getLocalizedMessage());
+        }
+        System.out.println(scifiII);
     }
 
 }
