4dba0fee4e62f065fa92172f769c38c2b46d3cf8
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-12-03 21:55:36 +0100

    RSS čtečka
    Načtení souborů - oprava metody

--- src/main/java/com/engeto/RSSctecka/controller/RSSController.java
+++ src/main/java/com/engeto/RSSctecka/controller/RSSController.java
@@ -19,23 +19,23 @@ public class RSSController {
 //    }
 
     @GetMapping("/scifi")
-    public static void text() {
+    public static void textScifi() {
         getText("scifi");
     }
 
     @GetMapping("/historic")
-    public static void text() {
+    public static void textHistoric() {
         getText("historic");
     }
 
     @GetMapping("/romantic")
-    public static void text() {
+    public static void textRomantic() {
         getText("romantic");
     }
 
 
     private static String getText(String fileName) {
-        File text;
+        File text = null;
         try {
             text = File.loadFromFile("/src/main/java/com/engeto/RSSctecka/"+fileName+".txt");
             return text.toString();
